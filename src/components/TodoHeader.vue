003136c4e06d9153a4718812af1cc1095e8f94c7
leeyerim <dpfla8628@gmailc.om>
2021-08-06 00:06:54 +0900

    todo-vue

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <h1>TODO it!</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+// 현재 컴포넌트의 엘리먼트에만 적용
+<style scoped> 
+  h1 {
+    color: #2F3B52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+  }
+</style>
